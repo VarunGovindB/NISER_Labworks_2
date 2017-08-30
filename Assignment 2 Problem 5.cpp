5caa70785243ad96edb5e73a6e69615c98143731
VarunGovindB <31276825+VarunGovindB@users.noreply.github.com>
2017-08-30 15:32:05 +0530

    Create Assignment 2 Problem 5.cpp

---
+++ Assignment 2 Problem 5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() 
+{
+int x,y;
+cout<<"Enter 2 integers:";
+cin>>x;
+cin>>y;
+cout<<x<<"\n";
+cout<<y;
+cout<<"\n";
+cout<<x<<"^"<<y<<"=";
+cout<<pow(x,y);
+return 0;
+}
