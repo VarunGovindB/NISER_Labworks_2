86f9950b0fdfa188bdd628996a5568f342a6e78c
VarunGovindB <31276825+VarunGovindB@users.noreply.github.com>
2017-08-30 15:40:08 +0530

    Create Assignment 2 Problem 9.cpp

---
+++ Assignment 2 Problem 9.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() 
+{
+float x;
+cout<<"Enter side of a equilateral triangle:";
+cin>>x;
+cout<<x<<"\n";
+cout<<1.732*x*x/4;
+return 0;
+}
