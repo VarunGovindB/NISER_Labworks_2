b5c64c13059704fb2fc9970b69ecf3a96178f9c9
VarunGovindB <31276825+VarunGovindB@users.noreply.github.com>
2017-08-30 15:24:17 +0530

    Create Assignment 2 Problem 4.cpp

---
+++ Assignment 2 Problem 4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+float days,weeks,years;
+cout<<"Enter number of days:";
+cin>>days;
+cout<<days<<"\n";
+weeks=days/7;
+years=days/365;
+cout<<"weeks:"<<weeks<<"\n";
+cout<<"years:"<<years;
+return 0;
+}
