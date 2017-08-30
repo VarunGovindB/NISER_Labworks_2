13bf1a2c656e8c83d6bd04b379977ad3577d616b
VarunGovindB <31276825+VarunGovindB@users.noreply.github.com>
2017-08-30 15:04:43 +0530

    Create Assignment 2 Problem 1.cpp

---
+++ Assignment 2 Problem 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+float length;
+cout<<"Enter length in centimeters:";
+cin>>length;
+cout<<length<<"\n";
+cout<<"Lenth in meters:"<<length/100<<"\n";
+cout<<"length in kilometers:"<<length/100000<<"\n";
+	return 0;
+}
