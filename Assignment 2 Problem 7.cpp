155346b573e8e67c3457d45d2417ad9956dd1662
VarunGovindB <31276825+VarunGovindB@users.noreply.github.com>
2017-08-30 15:36:00 +0530

    Create Assignment 2 Problem 7.cpp

---
+++ Assignment 2 Problem 7.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() 
+{
+float x,y;
+cout<<"Enter any 2 angles of a triangle:";
+cin>>x;
+cin>>y;
+cout<<x<<"\n";
+cout<<y<<"\n";
+cout<<180-(x+y);
+return 0;
+}
