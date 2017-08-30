f831f59f9e85171b026c983bddf6471fabc97d2c
VarunGovindB <31276825+VarunGovindB@users.noreply.github.com>
2017-08-30 15:37:36 +0530

    Create Assignment 2 Problem 8.cpp

---
+++ Assignment 2 Problem 8.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() 
+{
+float x,y;
+cout<<"Enter base and height of a triangle:";
+cin>>x;
+cin>>y;
+cout<<x<<"\n";
+cout<<y<<"\n";
+cout<<0.5*x*y;
+return 0;
+}
